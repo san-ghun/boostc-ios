a02c6eacf7c0f93ef12e2cba800e6ca8e8cc4ae5
Sanghun <po4865@gmail.com>
2022-02-17 20:53:18 +0900

    feat: Add ViewControllers to structure the UI of demo
    - Add LoginViewController & SignUpFirstViewController & SignUpSecondViewController.
    - Add UserInformation class as singleton to store user data.

---
+++ 2-SignUp/SignUp-demo/SignUp-demo/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  SignUp-demo
+//
+//  Created by Sanghun Park on 2022/02/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var titleImageView: UIImageView!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwdTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: - Methods
+    
+    // MARK: Custom Methods
+    
+    // MARK: IBActions
+    
+    // MARK: View with code
+    
+    
+}
+
---
+++ 2-SignUp/SignUp-demo/SignUp-demo/SignUpFirstViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpFirstViewController.swift
+//  SignUp-demo
+//
+//  Created by Sanghun Park on 2022/02/17.
+//
+
+import UIKit
+
+class SignUpFirstViewController: UIViewController {
+
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Methods
+    
+    // MARK: Custom Methods
+    
+    // MARK: IBActions
+    
+    // MARK: View with code
+    
+
+}
