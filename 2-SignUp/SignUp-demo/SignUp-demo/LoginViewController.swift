358757c5cf7f557a6f8d38333ac0733ce55aed84
Sanghun <po4865@gmail.com>
2022-02-17 22:08:18 +0900

    feat: Add IBOutlets view for LoginViewController
    - Add view of IBOutlet with code

--- 2-SignUp/SignUp-demo/SignUp-demo/LoginViewController.swift
+++ 2-SignUp/SignUp-demo/SignUp-demo/LoginViewController.swift
@@ -49,23 +49,111 @@ class LoginViewController: UIViewController {
     }
     
     func addTitleImageView() {
+        let imageView = UIImageView(image: UIImage(named: "apple"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(imageView)
+        
+        let centerX: NSLayoutConstraint = imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        let centerY: NSLayoutConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self.view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 0.8, constant: 0)
+        let width: NSLayoutConstraint = imageView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.6)
+        let ratio: NSLayoutConstraint = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 1)
+        
+        centerX.isActive = true
+        centerY.isActive = true
+        width.isActive = true
+        ratio.isActive = true
+        
+        self.titleImageView = imageView
     }
     
     func addIdTextField() {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
+        textField.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(textField)
+        
+        textField.textContentType = .username
+        textField.textAlignment = .left
+        textField.placeholder = "ID"
+        textField.autocapitalizationType = .none
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.backgroundColor = UIColor.white
+        textField.textColor = UIColor.black
+        
+        let centerX: NSLayoutConstraint = textField.centerXAnchor.constraint(equalTo: self.titleImageView.centerXAnchor)
+        let top: NSLayoutConstraint = textField.topAnchor.constraint(equalTo: self.titleImageView.bottomAnchor, constant: 8)
+        let left: NSLayoutConstraint = textField.leftAnchor.constraint(equalTo: self.titleImageView.leftAnchor, constant: 5)
+        
+        centerX.isActive = true
+        top.isActive = true
+        left.isActive = true
+        
+        self.idTextField = textField
     }
     
     func addPwdTextField() {
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
+        textField.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(textField)
+        
+        textField.textContentType = .password
+        textField.textAlignment = .left
+        textField.isSecureTextEntry = true
+        textField.placeholder = "Password"
+        textField.autocapitalizationType = .none
+        textField.borderStyle = UITextField.BorderStyle.roundedRect
+        textField.backgroundColor = UIColor.white
+        textField.textColor = UIColor.black
+        
+        let centerX: NSLayoutConstraint = textField.centerXAnchor.constraint(equalTo: self.titleImageView.centerXAnchor)
+        let top: NSLayoutConstraint = textField.topAnchor.constraint(equalTo: self.idTextField.bottomAnchor, constant: 5)
+        let left: NSLayoutConstraint = textField.leftAnchor.constraint(equalTo: self.titleImageView.leftAnchor, constant: 5)
+        
+        centerX.isActive = true
+        top.isActive = true
+        left.isActive = true
+        
+        self.pwdTextField = textField
     }
     
     func addSignInButton() {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(button)
+        
+        button.setTitle("Sign In", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        
+        let top: NSLayoutConstraint = button.topAnchor.constraint(equalTo: self.pwdTextField.bottomAnchor, constant: 5)
+        let left: NSLayoutConstraint = button.leftAnchor.constraint(equalTo: self.titleImageView.leftAnchor, constant: 20)
+        
+        top.isActive = true
+        left.isActive = true
+        
+        self.signInButton = button
     }
     
     func addSignUpButton() {
+        let button = UIButton(type: UIButton.ButtonType.custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(button)
+        
+        button.setTitle("Sign Up", for: .normal)
+        button.setTitleColor(UIColor.red, for: .normal)
+        
+        button.addTarget(self, action: #selector(touchUpSignUpButton), for: UIControl.Event.touchUpInside)
+        
+        let top: NSLayoutConstraint = button.topAnchor.constraint(equalTo: self.pwdTextField.bottomAnchor, constant: 5)
+        let right: NSLayoutConstraint = button.rightAnchor.constraint(equalTo: self.titleImageView.rightAnchor, constant: -20)
+        
+        top.isActive = true
+        right.isActive = true
+        
+        self.signUpButton = button
     }
     
 }
