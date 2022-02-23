c0c82eeeabe3e108c91fc47e94f3c3b1e80dddce
Sanghun <po4865@gmail.com>
2022-02-23 22:53:11 +0900

    feat: Build simple tutorial app to decode JSON data and show with TableView
    Add JSON data in Asset.
    Add TableView inside ViewController in Main.storyboard.
    Add Friends.swift and add Friend struct with Codable to handle JSON data.
    - add properties and enum CodingKeys with CodingKey to handle property when decode JSON
    Update ViewController.swift to decode JSON data and show in tableView.
    - Connect UITableView with storyboard
    - Update viewDidLoad() to get JSON dat from asset and decode JSON data
    - Add UITableViewDataSource to show data in tableViewCell

---
+++ 3-Weather/CodableWithJSON-tutorial/CodableWithJSON-tutorial/Friends.swift
@@ -0,0 +1,33 @@
+//
+//  Friends.swift
+//  CodableWithJSON-tutorial
+//
+//  Created by Sanghun Park on 2022/02/23.
+//
+
+import Foundation
+
+struct Friend: Codable {
+    
+    struct Address: Codable {
+        let country: String
+        let city: String
+    }
+    
+    let name: String
+    let age: Int
+    let addressInfo: Address
+    
+    var nameAndAge: String {
+        return self.name + "(\(self.age))"
+    }
+    
+    var fullAddress: String {
+        return self.addressInfo.city + ", " + self.addressInfo.country
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name, age
+        case addressInfo = "address_info"
+    }
+}
--- 3-Weather/CodableWithJSON-tutorial/CodableWithJSON-tutorial/ViewController.swift
+++ 3-Weather/CodableWithJSON-tutorial/CodableWithJSON-tutorial/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellIdentifier: String = "cell"
+    
+    var friends: [Friend] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.tableView.dataSource = self
+        
+        let jsonDecoder: JSONDecoder = JSONDecoder()
+        guard let dataAsset: NSDataAsset = NSDataAsset(name: "friends") else { return }
+        
+        do {
+            self.friends = try jsonDecoder.decode([Friend].self, from: dataAsset.data)
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+        
+        self.tableView.reloadData()
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let friend: Friend = self.friends[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = friend.nameAndAge
+        content.secondaryText = friend.fullAddress
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+}
