0af62b381acc1c18f585c068270c7b8db06207b5
Sanghun <po4865@gmail.com>
2022-02-23 20:40:51 +0900

    feat: Add TableView
    - Add TableView inside the ViewController in Main.storyboard
    	- TableView with Grouped in Style
    	- TableViewCell with Basic in Style
    - Update ViewController able to control UITableView
    	- Connect with UI component in storyboard
    	- Add data to show in table view
    	- Set delegate and datasource for TableView

--- 3-Weather/SimpleTable-tutorial/SimpleTable-tutorial/ViewController.swift
+++ 3-Weather/SimpleTable-tutorial/SimpleTable-tutorial/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let cellIdentifier: String = "cell"
+    
+    let korean: [String] = ["가", "나", "다", "라", "마", "바", "사", "아", "자", "차", "카", "타", "파", "하"]
+    let english: [String] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return korean.count
+        case 1:
+            return english.count
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = indexPath.section == 0 ? korean[indexPath.row] : english[indexPath.row]
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return section == 0 ? "한글" : "English"
+    }
+}
