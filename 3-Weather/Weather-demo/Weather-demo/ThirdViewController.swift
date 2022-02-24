c490abdbf856bcaa7e2a256887520b07585666df
Sanghun <po4865@gmail.com>
2022-02-24 16:57:30 +0900

    feat: Build UI for Window-3 and complete the demo
    - Add UI components to ThirdViewController in Main.storyboard
    - Connect IBOutlets and data to present

--- 3-Weather/Weather-demo/Weather-demo/ThirdViewController.swift
+++ 3-Weather/Weather-demo/Weather-demo/ThirdViewController.swift
@@ -10,13 +10,29 @@ import UIKit
 class ThirdViewController: UIViewController {
 
     // MARK: - Properties
+    var city: City?
     
     // MARK: IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var rainProbLabel: UILabel!
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationItem.title = self.city?.cityName
+        
+        self.imageView.image = UIImage(named: self.city!.stateImage)
+        self.stateLabel.text = self.city?.stateString
+        self.temperatureLabel.text = "\(self.city!.celsius) C / \(self.city!.fahrenheit) F"
+        self.rainProbLabel.text = "\(self.city!.rainfallProbability)%"
     }
     
     /*
