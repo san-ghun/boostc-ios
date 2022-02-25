f8d8f8903080d2c470060a539eb111eebbe52034
Sanghun <po4865@gmail.com>
2022-02-25 22:38:04 +0900

    feat: Build simple UI to download image and show in imageView
    - Add a imageView to show downloaded image and a button to execute download, in Main.storyboard
    - Add lines of code to execute download and show image in imageView in simple way, in ViewController.swift

--- 4-Album/AsyncExample-tutorial/AsyncExample-tutorial/ViewController.swift
+++ 4-Album/AsyncExample-tutorial/AsyncExample-tutorial/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var downloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
+    // MARK: - Methods
+    
+    // MARK: IBActions
+    @IBAction func touchUpDownloadButton(_ sender: UIButton) {
+        // 이미지 다운로드 -> 이미지 뷰에 셋팅
+        
+        guard let imageURL: URL = URL(string: "https://www.itl.cat/pngfile/big/292-2920819_large-madeira-portugal-hd.jpg") else { return }
+        
+        let imageData: Data = try! Data.init(contentsOf: imageURL)
+        guard let image: UIImage = UIImage(data: imageData) else { return }
+        
+        self.imageView.image = image
+    }
 }
 
