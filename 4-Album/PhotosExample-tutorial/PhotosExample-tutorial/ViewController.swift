07730a50475836338555b23086881a48195ca4a8
Sanghun <po4865@gmail.com>
2022-03-01 19:18:07 +0900

    feat: Update PhotosExample tutorial, add new ViewController to zoom the selected photo
    - Add new ViewController, ImageZoomViewController to segue from ViewController, to show selected photo and allow user to zoom photo.
    - Update Main.storyboard along side with the modifications.
    - Embed NavigationController to ViewController.
    - Add new ViewController as ImageZoomViewController.

--- 4-Album/PhotosExample-tutorial/PhotosExample-tutorial/ViewController.swift
+++ 4-Album/PhotosExample-tutorial/PhotosExample-tutorial/ViewController.swift
@@ -64,6 +64,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         PHPhotoLibrary.shared().register(self)
     }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        guard let nextVC: ImageZoomViewController = segue.destination as? ImageZoomViewController else { return }
+        
+        guard let cell: UITableViewCell = sender as? UITableViewCell else { return }
+        
+        guard let index: IndexPath = self.tableView.indexPath(for: cell) else { return }
+        
+        nextVC.asset = self.fetchResult[index.row]
+    }
 
     // MARK: - Methods
     
