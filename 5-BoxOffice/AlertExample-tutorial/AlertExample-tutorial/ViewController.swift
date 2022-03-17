22d3a6cea38d0529f20db8e21ba10268cbc221d6
Sanghun <po4865@gmail.com>
2022-03-17 14:50:59 +0900

    feat: Add buttons to show Alerts with alert style and actionSheet style
    ViewController.swift
    - Add showAlertController() that present simple alert on screen
    - Add IBActions, touchUpShowAlertButton() and touchUpShowActionSheetButton(), that call showAlertController() and show alerts with different style
    
    Main.storyboard
    - Add two buttons in ViewController along with IBActions in ViewController.swift

--- 5-BoxOffice/AlertExample-tutorial/AlertExample-tutorial/ViewController.swift
+++ 5-BoxOffice/AlertExample-tutorial/AlertExample-tutorial/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    
 
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    // MARK: - Methods
+    
+    // MARK: Custom Methods
+    func showAlertController(style: UIAlertController.Style) {
+        
+        let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: style)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (action: UIAlertAction) in
+            print("OK pressed")
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+        
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: {
+            print("Alert Controller shown")
+        })
+    }
+    
+    // MARK: IBActions
+    @IBAction func touchUpShowAlertButton(_ sender: UIButton) {
+        self.showAlertController(style: UIAlertController.Style.alert)
+    }
+    
+    @IBAction func touchUpShowActionSheetButton(_ sender: UIButton) {
+        self.showAlertController(style: UIAlertController.Style.actionSheet)
+    }
+    
 }
 
