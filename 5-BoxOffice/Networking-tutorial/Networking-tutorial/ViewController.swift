146915b0b01bf323b92b81d6c0a3fc50f91b4bde
Sanghun <po4865@gmail.com>
2022-03-24 19:13:50 +0900

    feat: Add Model and TableView
    - Add UITableView in Main.storyboard
    - Add struct Friend() and struct APIResponse() in Model.swift
    - Add lines of code to connect tableView in ViewController.swift

--- 5-BoxOffice/Networking-tutorial/Networking-tutorial/ViewController.swift
+++ 5-BoxOffice/Networking-tutorial/Networking-tutorial/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    let cellIdentifier: String = "friendCell"
+    var friends: [Friend] = []
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
 
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
     }
 
 
+    // MARK: Methods
+    
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
+        
+        let friend: Friend = self.friends[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = friend.name.full
+        content.secondaryText = friend.email
+        
+        //TODO: load image
+        
+        return cell
+    }
+    
+    
+}
