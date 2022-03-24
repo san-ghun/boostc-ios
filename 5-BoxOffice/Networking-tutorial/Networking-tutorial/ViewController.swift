85b469b6b68626753ef7a9c80aaf95c0dd266a91
Sanghun <po4865@gmail.com>
2022-03-24 19:32:21 +0900

    feat: (working) Enable to load data from URL
    - Update ViewController to load data from api through URL with URLSession
    - Add lines of code that add image in TableViewCell

--- 5-BoxOffice/Networking-tutorial/Networking-tutorial/ViewController.swift
+++ 5-BoxOffice/Networking-tutorial/Networking-tutorial/ViewController.swift
@@ -25,6 +25,33 @@ class ViewController: UIViewController {
         tableView.dataSource = self
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let url: URL = URL(string: "https://randomuser.me/api/?results=20&inc=name,email,picture") else { return }
+        
+        let session: URLSession = URLSession(configuration: .default)
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else { return }
+
+            do {
+                let apiResponse: APIResponse = try JSONDecoder().decode(APIResponse.self, from: data)
+                self.friends = apiResponse.results
+                // TODO: let tableView reload on main thread
+                self.tableView.reloadData()
+            } catch(let err) {
+                print(err.localizedDescription)
+            }
+        }
+        
+        dataTask.resume()
+    }
 
     // MARK: Methods
     
@@ -37,6 +64,7 @@ extension ViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath)
         
         let friend: Friend = self.friends[indexPath.row]
@@ -45,7 +73,9 @@ extension ViewController: UITableViewDataSource {
         content.text = friend.name.full
         content.secondaryText = friend.email
         
-        //TODO: load image
+        guard let imageURL: URL = URL(string: friend.picture.thumbnail) else { return cell }
+        guard let imageData: Data = try? Data(contentsOf: imageURL) else { return cell }
+        content.image = UIImage(data: imageData)
         
         return cell
     }
